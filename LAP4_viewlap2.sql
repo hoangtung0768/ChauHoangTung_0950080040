f0553ec29a09f6f07c4ae2406c00cdf1d9c8dbdb
hoangtung0768 <118713222+hoangtung0768@users.noreply.github.com>
2023-03-31 16:00:25 +0700

    Add files via upload

---
+++ LAP4_viewlap2.sql
@@ -0,0 +1,167 @@
+﻿USE  QLBANHANG;
+
+
+
+-- 1. Hiển thị thông tin các bảng dữ liệu trên --
+GO
+CREATE VIEW Cau1_LAP2 AS
+SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'
+GO
+SELECT * FROM Cau1_LAP2;
+-- 2.Đưa ra thông tin masp, tensp, tenhang,soluong, mausac, giaban, donvitinh, mota của các sản phẩm sắp xếp theo chiều giảm dần giá bán--
+GO
+CREATE VIEW Cau2_LAP2 AS
+SELECT Sanpham.masp, Sanpham.tensp, Hangsx.tenhang, Sanpham.soluong, Sanpham.mausac, Sanpham.giaban, Sanpham.donvitinh, Sanpham.mota
+FROM Sanpham
+INNER JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+ORDER BY Sanpham.giaban DESC;
+GO
+SELECT * FROM Cau2_LAP2;
+-- 3.Đưa ra thông tin các sản phẩm có trong cữa hàng do công ty có tên hãng là samsung sản xuất. --
+GO
+CREATE VIEW Cau3_LAP2 AS
+SELECT Sanpham.masp, Sanpham.tensp, Hangsx.tenhang, Sanpham.soluong, Sanpham.mausac, Sanpham.giaban, Sanpham.donvitinh, Sanpham.mota
+FROM Sanpham
+INNER JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+WHERE Hangsx.tenhang = 'Samsung'
+GO
+SELECT * FROM Cau3_LAP2;
+-- 4.Đưa ra thông tin các nhân viên Nữ ở phòng ‘Kế toán’.--
+GO
+CREATE VIEW Cau4_LAP2 AS
+SELECT * FROM nhanvien
+WHERE gioitinh = 'Nữ' AND phong = 'Kế toán'
+GO
+
+SELECT * FROM Cau4_LAP2;
+-- 5.Đưa ra thông tin phiếu nhập gồm: sohdn, masp, tensp, tenhang, soluongN, dongiaN, tiennhap=soluongN*dongiaN, mausac, donvitinh, ngaynhap, tennv, phong. Sắp xếp theo chiều tăng dần của hóa đơn nhập.--
+GO
+CREATE VIEW Cau5_LAP2 AS
+SELECT TOP 100 PERCENT Nhap.sohdn, Sanpham.masp,Sanpham.tensp, Hangsx.tenhang, Nhap.soluongN, Nhap.dongiaN, Nhap.soluongN*Nhap.dongiaN AS 
+tiennhap, Sanpham.mausac, Sanpham.donvitinh, Nhap.ngaynhap, Nhanvien.tennv, Nhanvien.phong
+FROM Nhap
+JOIN Sanpham ON Nhap.masp = Sanpham.masp
+JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+JOIN Nhanvien ON Nhap.manv = Nhanvien.manv
+ORDER BY Nhap.sohdn ASC;
+GO
+
+SELECT * FROM Cau5_LAP2;
+-- 6. --
+GO
+CREATE VIEW Cau6_LAP2 AS
+SELECT TOP 100 PERCENT Xuat.sohdx, Sanpham.masp, Sanpham.tensp, Hangsx.tenhang, Xuat.soluongX, Sanpham.giaban, 
+       Xuat.soluongX*Sanpham.giaban AS tienxuat, Sanpham.mausac, Sanpham.donvitinh, Xuat.ngayxuat, 
+       Nhanvien.tennv, Nhanvien.phong
+FROM Xuat
+INNER JOIN Sanpham ON Xuat.masp = Sanpham.masp
+INNER JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+INNER JOIN Nhanvien ON Xuat.manv = Nhanvien.manv
+WHERE MONTH(Xuat.ngayxuat) = 10 AND YEAR(Xuat.ngayxuat) = 2018
+ORDER BY Xuat.sohdx ASC;
+GO
+SELECT * FROM Cau6_LAP2;
+-- 7.  --
+GO
+CREATE VIEW Cau7_LAP2 AS
+SELECT TOP 100 PERCENT sohdn, Sanpham.masp, tensp, soluongN, dongiaN, ngaynhap, tennv, phong
+FROM Nhap 
+JOIN Sanpham ON Nhap.masp = Sanpham.masp 
+JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+JOIN Nhanvien ON Nhap.manv = Nhanvien.manv
+WHERE Hangsx.tenhang = 'Samsung' AND YEAR(ngaynhap) = 2017;
+GO
+SELECT * FROM Cau7_LAP2;
+--8. --
+GO
+CREATE VIEW Cau8_LAP2 AS
+SELECT TOP 10 Xuat.sohdx, Sanpham.tensp, Xuat.soluongX
+FROM Xuat 
+INNER JOIN Sanpham ON Xuat.masp = Sanpham.masp
+WHERE YEAR(Xuat.ngayxuat) = '2023' 
+ORDER BY Xuat.soluongX DESC;
+GO
+SELECT * FROM Cau8_LAP2;
+--9. --
+GO
+CREATE VIEW Cau9_LAP2 AS
+SELECT TOP 10 tenSP, giaBan
+FROM SanPham
+ORDER BY giaBan DESC;
+GO
+SELECT * FROM Cau9_LAP2;
+--10. --
+GO
+CREATE VIEW Cau10_LAP2 AS
+ SELECT Sanpham.masp, Sanpham.tensp, Sanpham.giaban, Hangsx.mahangsx AS mahangsx_hs, Hangsx.tenhang
+FROM Sanpham
+JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+WHERE Hangsx.tenhang = 'Samsung' AND Sanpham.giaban >= 100000 AND Sanpham.giaban <= 500000
+GO
+SELECT * FROM Cau10_LAP2;
+--11 --
+GO
+CREATE VIEW Cau11_LAP2 AS
+SELECT SUM(soluongN * dongiaN) AS tongtien
+FROM Nhap
+JOIN Sanpham ON Nhap.masp = Sanpham.masp
+JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+WHERE Hangsx.tenhang = 'Samsung' AND YEAR(ngaynhap) = 2018
+GO
+SELECT * FROM Cau11_LAP2;
+--12 --
+GO
+CREATE VIEW Cau12_LAP2 AS
+SELECT SUM(Xuat.soluongX * Sanpham.giaban) AS Tongtien
+FROM Xuat
+INNER JOIN Sanpham ON Xuat.masp = Sanpham.masp
+WHERE Xuat.ngayxuat = '2018-09-02'
+GO
+SELECT * FROM Cau12_LAP2;
+--13 --
+GO
+CREATE VIEW Cau13_LAP2 AS
+SELECT TOP 1 sohdn, ngaynhap, dongiaN
+FROM Nhap
+ORDER BY dongiaN DESC
+GO
+SELECT * FROM Cau1_LAP2;
+--14 --
+GO 
+CREATE VIEW Cau14_LAP2 AS
+SELECT TOP 10 Sanpham.tensp, SUM(Nhap.soluongN) AS TongSoLuongN 
+FROM Sanpham 
+INNER JOIN Nhap ON Sanpham.masp = Nhap.masp 
+WHERE YEAR(Nhap.ngaynhap) = 2019 
+GROUP BY Sanpham.tensp 
+ORDER BY TongSoLuongN DESC
+GO
+SELECT * FROM Cau14_LAP2;
+--15 --
+GO 
+CREATE VIEW Cau15_LAP2 AS
+SELECT Sanpham.masp, Sanpham.tensp
+FROM Sanpham
+INNER JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+INNER JOIN Nhap ON Sanpham.masp = Nhap.masp
+INNER JOIN Nhanvien ON Nhap.manv = Nhanvien.manv
+WHERE Hangsx.tenhang = 'Samsung' AND Nhanvien.manv = 'NV01';
+GO
+SELECT * FROM Cau15_LAP2;
+--16 --
+GO
+CREATE VIEW Cau16_LAP2 AS
+SELECT sohdn, masp, soluongN, ngaynhap
+FROM Nhap
+WHERE masp = 'SP02' AND manv = 'NV02'
+GO
+SELECT * FROM Cau16_LAP2;
+--17 --
+GO
+CREATE VIEW Cau17_LAP2 AS
+SELECT Nhanvien.manv, Nhanvien.tennv
+FROM Nhanvien
+INNER JOIN Xuat ON Nhanvien.manv = Xuat.manv
+WHERE Xuat.masp = 'SP02' AND Xuat.ngayxuat = '2020-03-02'
+GO
+SELECT * FROM Cau17_LAP2;
---
+++ LAP4_viewlap3.sql
@@ -0,0 +1,125 @@
+﻿-- 1 Specify xem mỗi hãng sản xuất có bao nhiêu loại sản phẩm--
+GO
+CREATE VIEW Cau1_LAP3 AS
+SELECT Hangsx.mahangsx, Hangsx.tenhang, COUNT(Sanpham.masp) AS SoLoaiSanPham
+FROM Hangsx
+LEFT JOIN Sanpham ON Hangsx.mahangsx = Sanpham.mahangsx
+GROUP BY Hangsx.mahangsx, Hangsx.tenhang;
+GO
+
+SELECT * FROM Cau1_LAP3;
+-- 2 Hãy thống kê xem tổng tiền nhập của từng sản phẩm trong năm 2018--
+GO
+CREATE VIEW Cau2_LAP3 AS
+SELECT Sanpham.masp, Sanpham.tensp, SUM(Nhap.soluongN * Nhap.dongiaN) AS TongTienNhap
+FROM Sanpham
+INNER JOIN Nhap ON Sanpham.masp = Nhap.masp
+WHERE YEAR(Nhap.ngaynhap) = 2018
+GROUP BY Sanpham.masp, Sanpham.tensp;
+GO
+
+SELECT * FROM Cau2_LAP3;
+-- 3 Hãy thống kê các sản phẩm có tổng sản lượng năm 2018 lớn hơn 10.000 sản phẩm--
+-- sản phẩm của hãng samsung.--
+GO
+CREATE VIEW Cau3_LAP3 AS
+SELECT Sanpham.masp, Sanpham.tensp, SUM(Xuat.soluongX) AS TongSoLuongXuat
+FROM Sanpham
+INNER JOIN Xuat ON Sanpham.masp = Xuat.masp
+WHERE YEAR(Xuat.ngayxuat) = 2018 AND Sanpham.mahangsx = 'SAMSUNG' 
+GROUP BY Sanpham.masp, Sanpham.tensp
+HAVING SUM(Xuat.soluongX) > 10000;
+GO
+
+SELECT * FROM Cau3_LAP3;
+-- 4 Thống kê số lượng nhân viên Nam của mỗi phòng ban.--
+GO
+CREATE VIEW Cau4_LAP3 AS
+SELECT Nhanvien.phong, COUNT(*) AS SoLuongNhanVienNam
+FROM Nhanvien
+WHERE Nhanvien.gioitinh = N'Nam'
+GROUP BY Nhanvien.phong;
+GO
+
+SELECT * FROM Cau4_LAP3;
+-- 5 Thống kê tổng lượng nhập của mỗi hãng sản xuất trong năm 2018.--
+GO
+CREATE VIEW Cau5_LAP3 AS
+SELECT Hangsx.tenhang, SUM(Nhap.soluongN) AS TongSoLuongNhap
+FROM Hangsx
+INNER JOIN Sanpham ON Hangsx.mahangsx = Sanpham.mahangsx
+INNER JOIN Nhap ON Sanpham.masp = Nhap.masp
+WHERE YEAR(Nhap.ngaynhap) = 2018
+GROUP BY Hangsx.tenhang
+GO
+
+SELECT * FROM Cau5_LAP3;
+-- 6 Hãy xem thống kê tổng số tiền xuất ra của mỗi nhân viên trong năm 2018 là bao--
+GO
+CREATE VIEW Cau6_LAP3 AS
+SELECT Nhanvien.tennv, SUM(Xuat.soluongX * Sanpham.giaban) AS TongTienXuat
+FROM Nhanvien
+INNER JOIN Xuat ON Nhanvien.manv = Xuat.manv
+INNER JOIN Sanpham ON Xuat.masp = Sanpham.masp
+WHERE YEAR(Xuat.ngayxuat) = 2018
+GROUP BY Nhanvien.tennv
+GO
+
+SELECT * FROM Cau6_LAP3;
+-- 7 Please đưa ra tổng số tiền đầu vào của mỗi nhân viên trong tháng 8 – năm 2018 có tổng giá trị--
+--lớn hơn 100.000--
+GO
+CREATE VIEW Cau7_LAP3 AS
+SELECT Nhanvien.tennv, SUM(Nhap.soluongN * Nhap.dongiaN) AS TongTienNhap
+FROM Nhanvien
+INNER JOIN Nhap ON Nhanvien.manv = Nhap.manv
+WHERE YEAR(Nhap.ngaynhap) = 2018 AND MONTH(Nhap.ngaynhap) = 8
+GROUP BY Nhanvien.tennv
+HAVING SUM(Nhap.soluongN * Nhap.dongiaN) > 100000
+GO
+
+SELECT * FROM Cau7_LAP3;
+-- 8 Please đưa ra danh sách các sản phẩm đã nhập nhưng chưa xuất bao giờ.--
+GO
+CREATE VIEW Cau8_LAP3 AS
+SELECT Sanpham.masp, Sanpham.tensp, Sanpham.soluong, Sanpham.mausac, Sanpham.giaban, Sanpham.donvitinh, Sanpham.mota
+FROM Sanpham
+LEFT JOIN Xuat ON Sanpham.masp = Xuat.masp
+WHERE Xuat.masp IS NULL
+
+GO
+
+SELECT * FROM Cau8_LAP3;
+-- 9 Please đưa ra danh sách các sản phẩm đã nhập vào năm 2018 và xuất hiện vào năm 2018.--
+GO
+CREATE VIEW Cau9_LAP3 AS
+SELECT Sanpham.masp, Sanpham.tensp, Sanpham.soluong, Sanpham.mausac, Sanpham.giaban, Sanpham.donvitinh, Sanpham.mota
+FROM Sanpham
+INNER JOIN Nhap ON Sanpham.masp = Nhap.masp
+INNER JOIN Xuat ON Sanpham.masp = Xuat.masp
+WHERE YEAR(Nhap.ngaynhap) = 2018 AND YEAR(Xuat.ngayxuat) = 2018
+GO
+
+SELECT * FROM Cau9_LAP3;
+-- 10 Please đưa ra danh sách các nhân viên vừa nhập vừa xuất.--
+GO
+CREATE VIEW Cau10_LAP3 AS
+SELECT Nhanvien.manv, Nhanvien.tennv
+FROM Nhanvien
+INNER JOIN Nhap ON Nhanvien.manv = Nhap.manv
+INNER JOIN Xuat ON Nhanvien.manv = Xuat.manv
+GO
+
+SELECT * FROM Cau10_LAP3;
+-- 11 Hãy đưa ra danh sách các nhân viên không tham gia công việc nhập và xuất.--
+go
+CREATE VIEW Cau11_LAP3 AS
+SELECT nv.manv, nv.tennv
+FROM Nhanvien nv
+WHERE nv.manv NOT IN (
+    SELECT DISTINCT manv FROM Nhap
+    UNION
+    SELECT DISTINCT manv FROM Xuat
+)
+go
+SELECT * FROM Cau11_LAP3;
